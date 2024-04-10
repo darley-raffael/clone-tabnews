ed01a530a45040b187813dceb3b67004041c6f99
Dárley Raffael <darleyraffael@gmail.com>
2024-04-09 22:36:18 -0300

    add `.env` and `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT NOW() as now;");
+  console.log(result);
   return response.status(200).json({ message: "OK" });
 }
